069d2ad08418bbcc8b663a749d10977bdcf8e174
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2025-12-12 12:21:10 +0100

    use react router

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
 import { validateUser } from "../utils/index.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
   try {
@@ -13,13 +14,27 @@ export const register = async (req, res) => {
 
     const { name, email, password } = req.body;
 
+    const existingUser = await User.findOne({
+      email,
+    });
 
-  
+    if (existingUser) {
+      return res.status(400).json({
+        error: "Invalid Email, user already exist",
+      });
+    }
 
+    // hash the password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
 
-    
-  } catch (error) {
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
-
-  }
+    const result = await user.save();
+    res.json(result);
+  } catch (error) {}
 };
